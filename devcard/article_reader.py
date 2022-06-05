8cf79fccbe86e5da24c3656c2655862fb5548f5a
Scott Draper <admin@scottdraper.io>
2022-06-04 21:54:27 -0600

    Attempt to overcome GitHub device verification via email

--- devcard/article_reader.py
+++ devcard/article_reader.py
@@ -3,12 +3,37 @@
 import os
 import time
 import json
+import smtplib
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+# ---------------------------------------------------------------------------------------------- #
+
+
+# CONFIG EMAIL CAPABILITIES
+# ---------------------------------------------------------------------------------------------- #
+def send_email(subject, body):
+    sender = E_U_NAME
+    receivers = ['admin@scottdraper.io']
+    message = f'''
+    From: Bot Draper <{E_U_NAME}>
+    To: Scott Draper <{E_U_NAME}>
+    Subject: {subject}
+
+    {body}
+    '''
+
+    try:
+        smtp_server = smtplib.SMTP_SSL('hs1.name.tools', 465)
+        smtp_server.ehlo()
+        smtp_server.login(E_U_NAME, E_PASS_KEY)
+        smtp_server.sendmail(sender, receivers, message)
+        smtp_server.close()
+    except Exception as ex:
+        print ('Email was not delivered:', '\n', ex)
 # ---------------------------------------------------------------------------------------------- #
 
 
@@ -23,12 +48,16 @@ opts.add_argument('--disable-dev-shm-usage')
 if os.environ.get('ON_HEROKU'):
     opts.add_argument('--headless')
     opts.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
-    U_NAME = os.environ.get('U_NAME')
-    PASS_KEY = os.environ.get('PASS_KEY')
+    E_U_NAME = os.environ.get('E_U_NAME')
+    G_U_NAME = os.environ.get('G_U_NAME')
+    E_PASS_KEY = os.environ.get('E_PASS_KEY')
+    G_PASS_KEY = os.environ.get('G_PASS_KEY')
 else:
     opts.binary_location = 'C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe'
-    U_NAME = json.load(open(f'{os.path.dirname(os.path.realpath(__file__))}\\credentials.json'))['u']
-    PASS_KEY = json.load(open(f'{os.path.dirname(os.path.realpath(__file__))}\\credentials.json'))['p']
+    E_U_NAME = json.load(open(f'{os.path.dirname(os.path.realpath(__file__))}\\credentials.json'))['email']['u']
+    G_U_NAME = json.load(open(f'{os.path.dirname(os.path.realpath(__file__))}\\credentials.json'))['github']['u']
+    E_PASS_KEY = json.load(open(f'{os.path.dirname(os.path.realpath(__file__))}\\credentials.json'))['email']['p']
+    G_PASS_KEY = json.load(open(f'{os.path.dirname(os.path.realpath(__file__))}\\credentials.json'))['github']['p']
 # ---------------------------------------------------------------------------------------------- #
 opts.add_argument('--headless') #TODO remove after development
 
@@ -51,8 +80,8 @@ driver.find_element(by=By.XPATH, value='//span[text()="Access all features"]').c
 WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(by=By.XPATH, value='(//button/span)[13]'))
 driver.find_element(by=By.XPATH, value='(//button/span)[13]').click()
 WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(by=By.XPATH, value='//input[@name="login"]'))
-driver.find_element(by=By.XPATH, value='//input[@name="login"]').send_keys(U_NAME)
-driver.find_element(by=By.XPATH, value='//input[@type="password"]').send_keys(PASS_KEY)
+driver.find_element(by=By.XPATH, value='//input[@name="login"]').send_keys(G_U_NAME)
+driver.find_element(by=By.XPATH, value='//input[@type="password"]').send_keys(G_PASS_KEY)
 driver.find_element(by=By.XPATH, value='//input[@type="submit"]').click()
 
 # overcome GitHub's device verification
@@ -60,8 +89,13 @@ try:
     WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(by=By.XPATH, value='//img[@alt="scottdraper\'s profile"]'))
 except:
     WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(by=By.XPATH, value='//h1[@data-test-selector="github-mobile-challenge"]'))
-    print(driver.find_element(by=By.XPATH, value='//h1[@data-test-selector="github-mobile-challenge"]').text)
-    WebDriverWait(driver, timeout=60).until(lambda d: d.find_element(by=By.XPATH, value='//img[@alt="scottdraper\'s profile"]'))
+    auth_code = driver.find_element(by=By.XPATH, value='//h1[@data-test-selector="github-mobile-challenge"]').text
+    send_email('GitHub Auth Code', f'''
+        Your Daily.dev auto-article-reader is attempting to login to Daily.dev via GitHub. 
+        Enter the following code into your GitHub mobile app: {auth_code} \n
+        This code expires in 10 minutes.
+    ''')
+    WebDriverWait(driver, timeout=600).until(lambda d: d.find_element(by=By.XPATH, value='//img[@alt="scottdraper\'s profile"]'))
 # ---------------------------------------------------------------------------------------------- #
 
 
