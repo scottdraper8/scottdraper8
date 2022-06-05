598fc1b31a9cf638481bc8d62654f096bc7e2d2d
Scott Draper <admin@scottdraper.io>
2022-06-05 00:32:57 -0600

    Added article collection and reading logic
    article_reader.py now attempts to collect up to 1000 article URLs from the daily.dev feed. It then opens all collected URLs (even if the amount is less than 1000) in sets of 21.

--- devcard/article_reader.py
+++ devcard/article_reader.py
@@ -1,11 +1,13 @@
 # DEPENDENCIES
 # ---------------------------------------------------------------------------------------------- #
 import os
+import sys
 import time
 import json
 import smtplib
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
@@ -41,6 +43,7 @@ def send_email(subject, body):
 # ---------------------------------------------------------------------------------------------- #
 opts = Options()
 opts.add_argument('--no-sandbox')
+opts.add_argument('--log-level=3')
 opts.add_argument('--disable-gpu')
 opts.add_argument("--start-maximized")
 opts.add_argument("--window-size=1920,1080")
@@ -53,11 +56,12 @@ if os.environ.get('ON_HEROKU'):
     E_PASS_KEY = os.environ.get('E_PASS_KEY')
     G_PASS_KEY = os.environ.get('G_PASS_KEY')
 else:
+    parent_dir = os.path.dirname(os.path.realpath(__file__))
     opts.binary_location = 'C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe'
-    E_U_NAME = json.load(open(f'{os.path.dirname(os.path.realpath(__file__))}\\credentials.json'))['email']['u']
-    G_U_NAME = json.load(open(f'{os.path.dirname(os.path.realpath(__file__))}\\credentials.json'))['github']['u']
-    E_PASS_KEY = json.load(open(f'{os.path.dirname(os.path.realpath(__file__))}\\credentials.json'))['email']['p']
-    G_PASS_KEY = json.load(open(f'{os.path.dirname(os.path.realpath(__file__))}\\credentials.json'))['github']['p']
+    E_U_NAME = json.load(open(f'{parent_dir}\\credentials.json'))['email']['u']
+    G_U_NAME = json.load(open(f'{parent_dir}\\credentials.json'))['github']['u']
+    E_PASS_KEY = json.load(open(f'{parent_dir}\\credentials.json'))['email']['p']
+    G_PASS_KEY = json.load(open(f'{parent_dir}\\credentials.json'))['github']['p']
 # ---------------------------------------------------------------------------------------------- #
 opts.add_argument('--headless') #TODO remove after development
 
@@ -75,28 +79,87 @@ if not os.environ.get('ON_HEROKU'):
 
 # LOG IN TO DAILY.DEV VIA GITHUB
 # ---------------------------------------------------------------------------------------------- #
-WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(by=By.XPATH, value='//span[text()="Access all features"]'))
-driver.find_element(by=By.XPATH, value='//span[text()="Access all features"]').click()
-WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(by=By.XPATH, value='(//button/span)[13]'))
-driver.find_element(by=By.XPATH, value='(//button/span)[13]').click()
-WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(by=By.XPATH, value='//input[@name="login"]'))
-driver.find_element(by=By.XPATH, value='//input[@name="login"]').send_keys(G_U_NAME)
-driver.find_element(by=By.XPATH, value='//input[@type="password"]').send_keys(G_PASS_KEY)
-driver.find_element(by=By.XPATH, value='//input[@type="submit"]').click()
-
-# overcome GitHub's device verification
 try:
-    WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(by=By.XPATH, value='//img[@alt="scottdraper\'s profile"]'))
-except:
-    WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(by=By.XPATH, value='//h1[@data-test-selector="github-mobile-challenge"]'))
-    auth_code = driver.find_element(by=By.XPATH, value='//h1[@data-test-selector="github-mobile-challenge"]').text
-    send_email('GitHub Auth Code', f'''
-        Your Daily.dev auto-article-reader is attempting to login to Daily.dev via GitHub. 
-        Enter the following code into your GitHub mobile app: {auth_code} \n
-        This code expires in 10 minutes.
-    ''')
-    WebDriverWait(driver, timeout=600).until(lambda d: d.find_element(by=By.XPATH, value='//img[@alt="scottdraper\'s profile"]'))
+    WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(by=By.XPATH, value='//span[text()="Access all features"]'))
+    driver.find_element(by=By.XPATH, value='//span[text()="Access all features"]').click()
+    WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(by=By.XPATH, value='(//button/span)[13]'))
+    driver.find_element(by=By.XPATH, value='(//button/span)[13]').click()
+    WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(by=By.XPATH, value='//input[@name="login"]'))
+    driver.find_element(by=By.XPATH, value='//input[@name="login"]').send_keys(G_U_NAME)
+    driver.find_element(by=By.XPATH, value='//input[@type="password"]').send_keys(G_PASS_KEY)
+    driver.find_element(by=By.XPATH, value='//input[@type="submit"]').click()
+
+    # overcome GitHub's device verification
+    try:
+        WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(by=By.XPATH, value='//img[@alt="scottdraper\'s profile"]'))
+    except:
+        WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(by=By.XPATH, value='//h1[@data-test-selector="github-mobile-challenge"]'))
+        auth_code = driver.find_element(by=By.XPATH, value='//h1[@data-test-selector="github-mobile-challenge"]').text
+        send_email('GitHub Auth Code', 
+            f'''Your Daily.dev Auto Article Reader is attempting to login to Daily.dev via GitHub. 
+            Enter the following code into your GitHub mobile app: {auth_code} \n
+            This code expires in 10 minutes.''')
+        WebDriverWait(driver, timeout=600).until(lambda d: d.find_element(by=By.XPATH, value='//img[@alt="scottdraper\'s profile"]'))
+except Exception as e:
+    send_email('Daily.dev Auto Article Reader Failure', 
+        f'''Your Daily.dev Auto Article Reader was unable to login to Daily.dev. 
+        Error Message:\n{e}''')
+    driver.quit()
+    sys.exit()
 # ---------------------------------------------------------------------------------------------- #
 
 
+# COLLECT ARTICLES TO READ
+# ---------------------------------------------------------------------------------------------- #
+article_urls = []
+try:
+    driver.get('https://app.daily.dev/my-feed')
+    WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(by=By.XPATH, value='//img[@alt="scottdraper\'s profile"]'))
+    no_change_count = 0
+    while len(article_urls) < 1000:
+        if no_change_count == 1:
+            print(f'Collected {len(article_urls)} articles')
+        for url in driver.find_elements(by=By.XPATH, value='//article/a'):
+            if url.get_attribute('href') not in article_urls and url.get_attribute('href').__contains__('api.daily.dev'):
+                article_urls.append(url.get_attribute('href'))
+                no_change_count = 0
+        driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.PAGE_DOWN)
+        no_change_count += 1
+        if no_change_count == 5:
+            break
+except Exception as e:
+    send_email('Daily.dev Auto Article Reader Failure', 
+        f'''Your Daily.dev Auto Article Reader was unable to collect the designated number of articles to read.
+        {len(article_urls)} of 1000 articles were collected. 
+        Error Message:\n{e}''')
+    driver.quit()
+    sys.exit()
+# ---------------------------------------------------------------------------------------------- #
+
+
+# READ COLLECTED ARTICLES
+# ---------------------------------------------------------------------------------------------- #
+try:
+    [driver.execute_script(f'window.open("about:blank", "tab{x}");') for x in range(0, 21)]
+    for i, url in enumerate(article_urls):
+        if i % 20 == 0 and i > 0:
+            current_urls = [article_urls[i] for i in range(i - 20, i + 1)]
+            for x, link in enumerate(current_urls):
+                driver.switch_to.window(f'tab{x}')
+                driver.get(link)
+                print(f'URL {(i - 20) + x} of 60', driver.current_url)
+except Exception as e:
+    send_email('Daily.dev Auto Article Reader Failure', 
+        f'''Your Daily.dev Auto Article Reader was unable to read all the collected articles.
+        Error Message:\n{e}''')
+    driver.quit()
+    sys.exit()
+# ---------------------------------------------------------------------------------------------- #
+
+
+# FINISH AND REPORT
+# ---------------------------------------------------------------------------------------------- #
+send_email('Daily.dev Auto Article Reader Success', 
+        f'Your Daily.dev Auto Article Reader collected and a read a total of {len(article_urls)} articles. Great work!')
 driver.quit()
+# ---------------------------------------------------------------------------------------------- #
